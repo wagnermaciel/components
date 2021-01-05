5b14c4e650eccb28bd9ba98606daf2f83eecf7c7
wagnermaciel <wagnermaciel@google.com>
2021-01-05 10:12:42 -0800

    feat(material-experimental/mdc-slider): add todo for e2e tests

--- src/material-experimental/mdc-slider/slider.e2e.spec.ts
+++ src/material-experimental/mdc-slider/slider.e2e.spec.ts
@@ -5,3 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+
+describe('mat-slider', () => {
+  // TODO(wagnermaciel): Implement this in a separate PR
+});
